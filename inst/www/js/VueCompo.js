cecd470dfbc50fcda113606f85ee4e3da4ae793e
Jean-Eudes Hollebecq <jehollebecq@gmail.com>
2020-03-20 16:43:07 +0100

    removing stuff

--- inst/www/js/VueCompo.js
+++ inst/www/js/VueCompo.js
@@ -4,21 +4,12 @@ ocpu.seturl("http://0.0.0.0:8004/ocpu/library/networkVisu/R");
 var App = new Vue({
   el: "#exploreApp",
   data: {
-    wsParams: {
-      RfunctionName: "installationTable",
-      /* name: ["Diaphen", "OpensilexDemo", "Agrophen", "Pheno3C", "Phenovia", "PhenoField", "Ephesia"],
-      api: ["147.100.175.121:8080/phenomeDiaphenAPI/rest/", "opensilex.org/openSilexAPI/rest/", "147.100.175.121:8080/phenomeAgrophenAPI/rest/", "147.100.175.121:8080/phenomePheno3cAPI/rest/", "147.100.175.121:8080/phenomePhenoviaAPI/rest/", "147.100.175.121:8080/phenomePhenofieldAPI/rest/", "138.102.159.36:8080/phenomeEphesiaAPI/rest/"],
-     */  
-      name: ["Diaphen", "OpensilexDemo", "Ephesia"],
-      api: ["147.100.175.121:8080/phenomeDiaphenAPI/rest/", "opensilex.org/openSilexAPI/rest/",  "138.102.159.36:8080/phenomeEphesiaAPI/rest/"], 
-           
-
-    },
     selected: [],
     collectedData: {
       INST: [{ api: "FALSE", name: "All" }],
-      computedDF: [],
-      refinedDF: [],
+      computedDF: [{ key: "nom", value: 2 },
+                   { key: "nom", value: 8 }  
+                  ],
       functionName: "collectScientificObject"
     },
     graphParameters: {
@@ -29,27 +20,6 @@ var App = new Vue({
         parameterOfInterest: "parameterOfInterest",
         filteredInstallation: "filteredInstallation",
         groupBy: "groupBy"
-      },
-      piechart: {
-        functionName: "pieGraph",
-        parameterOfInterest: "parameterOfInterest",
-        filteredInstallation: "filteredInstallation"
-      },
-      treemap: {
-        functionName: "treemapGraph",
-        parameterOfInterest: "parameterOfInterest",
-        groupBy: "groupBy"
-      },
-      boxplot: {
-        functionName: "boxplotGraph",
-        parameterOfInterest: "parameterOfInterest",
-        filteredInstallation: "filteredInstallation"
-      },
-      radar: {
-        functionName: "radarGraph",
-        parameterOfInterest: "parameterOfInterest",
-        groupBy: "groupBy",
-        outputName: "Graph.html"
       }
     },
     exportedData: {
@@ -62,52 +32,21 @@ var App = new Vue({
       searchedParameters: "searchedParameters"
     },
     tabs: { 
-      activetab: 1,
+      activetab: 2,
       loading: true 
     }
   },
   computed: {
-    INST: function () {
-      var inst = [];
-      for (var j = 0; j < this.wsParams.name.length; j++) {
-        instance = { name: this.wsParams.name[j], api: this.wsParams.api[j] };
-        inst.push(instance)
-      }
-      return inst
-    },
-    refineDF: function () {
-      var self = this;
-      var data = self.collectedData.computedDF;
-      var format = self.exportedData.format;
-      var filename = self.exportedData.filename;
-      return ocpu.rpc(
-        self.exportedData.functionName,
-        {
-          DATA: data,
-          format: format,
-          filename: filename,
-          rawData: 'FALSE'
-        },
-        function (df) {
-          self.collectedData.refinedDF = df
-          
-        }
-      ).fail(function (request) {
-        alert("Error: " + request.responseText);
-      });
-    },
+    /* comme data mais recalculé quand l'input change */
   },
-  mounted: function () {
-    this.fillListInput(inputId = this.graphParameters.barplot.filteredInstallation, inputList = this.wsParams.name);
-    this.collectData();
+  mounted: {
+    /* fonction à lancer au démarage de l'app */
   },
+  /* les "variables de vue" */
   methods: {
+    /* un exemple de fonction avec ocpu.rpc c'est la fonction de base */
     installationTable: function () {
       var self = this;
-      installationTable = [
-        self.wsParams.name,
-        self.wsParams.api
-      ]
       return ocpu.rpc(
         //Create array of variables' options
         // R function name
@@ -129,66 +68,29 @@ var App = new Vue({
         alert("Error: " + request.responseText);
       });
     },
-    fillListInput: function (inputId, inputList) {
-      inputData = [];
-      inputList.forEach(function (inputItem) {
-        item = {};
-        item.id = inputItem;
-        item.text = inputItem;
-        inputData.push(item);
-      });
-      // console.log(inputData);
-      defaultSelectParameters = {
-        data: inputData
-      };
-      // merge objects
-      finalSelectParameters = { ...defaultSelectParameters };
-      $("#" + inputId).select2(finalSelectParameters);
-    },
-    collectData: function () {
-      document.getElementById("spinner").style.visibility = false;
-      var self = this;
-      self.installationTable()
-      // Fill variables
-      // the arguments of the function ocpu.rpc are findable here :
-      // https://www.opencpu.org/jslib.html#lib-jsonrpc
-      return ocpu.rpc(
-        //Create array of variables' options
-        // R function name
-        self.collectedData.functionName,
-        // list of arguments names and value
-        {
-          inst: self.INST
-        },
-
-        function (output) {
-          self.collectedData.computedDF = output
-          document.getElementById("spinner").style.visibility = "hidden";
-          self.tabs.loading = false
-          return output;
-        }
-      ).fail(function (request) {
-        $("#cssLoader").removeClass("is-active");
-        alert("Error: " + request.responseText);
-      });
-    },
+    /* un exemple avec un .rplot */
     showbarGraph: function () {
       $("#cssLoader").addClass("is-active");
+      /* petite astuce pour que le this marche à l'intérieur d'une fonction */
       var self = this;
       // Run the R function
+      /* ici je récupère les valeurs des paramètres, différentes manières */
       var parameterOfInterest = $("#" + this.graphParameters.barplot.parameterOfInterest).val();
-      var filteredInstallation = $("#" + self.graphParameters.barplot.filteredInstallation).val();
-      var groupBy = $("#" + self.graphParameters.barplot.groupBy).val();
+      var filteredInstallation = $("#select_id_1").val();
+      var groupBy = "All"
       var outputName = self.graphParameters.outputName;
       var iframeInput = self.graphParameters.iframeInput;
       return (req = $(iframeInput).rplot(
+        /* nom de la fonction */
         self.graphParameters.barplot.functionName,
+        /* arguments et valeurs des arguments */
         {
           computedDF: self.collectedData.computedDF,
           parameterOfInterest: parameterOfInterest,
           filteredInstallation: filteredInstallation,
           groupBy: groupBy
         },
+        /* actions à faire lorsque réponse */
         function (session) {
           $("#" + iframeInput).attr(
             "src",
@@ -203,93 +105,7 @@ var App = new Vue({
         })
       );
     },
-    showpieChart: function () {
-      $("#cssLoader").addClass("is-active");
-      var self = this;
-      // Run the R function
-      var parameterOfInterest = $("#" + self.graphParameters.piechart.parameterOfInterest).val();
-      var filteredInstallation = $("#" + self.graphParameters.piechart.filteredInstallation).val();
-      var outputName = this.graphParameters.outputName;
-      var iframeInput = this.graphParameters.iframeInput;
-      return (req = $(iframeInput).rplot(
-        self.graphParameters.piechart.functionName,
-        {
-          computedDF: self.collectedData.computedDF,
-          parameterOfInterest: parameterOfInterest,
-          filteredInstallation: filteredInstallation
-        },
-        function (session) {
-          $("#" + iframeInput).attr(
-            "src",
-            session.getFileURL(outputName)
-          );
-          $("#submit").removeAttr("disabled");
-          $("#cssLoader").removeClass("is-active");
-        }).fail(function (request) {
-          $("#submit").removeAttr("disabled");
-          $("#cssLoader").removeClass("is-active");
-          alert("An unknown error has append : " + request.responseText);
-        })
-      );
-    },
-    treemapGraph: function () {
-      $("#cssLoader").addClass("is-active");
-      var self = this;
-      // Run the R function
-      var class1 = $("#" + self.graphParameters.treemap.parameterOfInterest).val();
-      var class2 = $("#" + self.graphParameters.treemap.groupBy).val();
-      var outputName = this.graphParameters.outputName;
-      var iframeInput = this.graphParameters.iframeInput;
-      return (req = $(iframeInput).rplot(
-        self.graphParameters.treemap.functionName,
-        {
-          computedDF: self.collectedData.computedDF,
-          class1: class1,
-          class2: class2
-        },
-        function (session) {
-          $("#" + iframeInput).attr(
-            "src",
-            session.getFileURL(outputName)
-          );
-          $("#submit").removeAttr("disabled");
-          $("#cssLoader").removeClass("is-active");
-        }).fail(function (request) {
-          $("#submit").removeAttr("disabled");
-          $("#cssLoader").removeClass("is-active");
-          alert("An unknown error has append : " + request.responseText);
-        })
-      );
-    },
-    showboxplot: function () {
-      $("#cssLoader").addClass("is-active");
-      var self = this;
-      // Run the R function
-      var parameterOfInterest = $("#" + self.graphParameters.boxplot.parameterOfInterest).val();
-      var filteredInstallation = $("#" + self.graphParameters.boxplot.filteredInstallation).val();
-      var outputName = this.graphParameters.outputName;
-      var iframeInput = this.graphParameters.iframeInput;
-      return (req = $(iframeInput).rplot(
-        self.graphParameters.boxplot.functionName,
-        {
-          computedDF: self.collectedData.computedDF,
-          parameterOfInterest: parameterOfInterest,
-          filteredInstallation: filteredInstallation
-        },
-        function (session) {
-          $("#" + iframeInput).attr(
-            "src",
-            session.getFileURL(outputName)
-          );
-          $("#submit").removeAttr("disabled");
-          $("#cssLoader").removeClass("is-active");
-        }).fail(function (request) {
-          $("#submit").removeAttr("disabled");
-          $("#cssLoader").removeClass("is-active");
-          alert("An unknown error has append : " + request.responseText);
-        })
-      );
-    },
+    /* un exemple avec ocpu.call */
     showradar: function () {
       $("#cssLoader").addClass("is-active");
       var self = this;
@@ -327,87 +143,17 @@ var App = new Vue({
       hiddenElement.click();
     },
     download_json: function () {
-      var raw = this.exportedData.rawData;
-      if (raw === 'false') {
-        var DATA = this.collectedData.refinedDF;
-      } else {
-        var DATA = this.collectedData.computedDF;
-      }
+      var DATA = this.collectedData.computedDF;
       var hiddenElement = document.getElementById('Download');
       hiddenElement.href = 'data:json/application;charset=utf-8,' + JSON.stringify(DATA);
       hiddenElement.target = '_blank';
       hiddenElement.download = 'file.json';
       hiddenElement.click();
     },
-    convertToCSV: function (objArray) {
-      var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-      var str = '';
-      var fields = Object.keys(array[0]);
-      for (var i = 0; i < array.length; i++) {
-        var line = '';
-        for (var index in array[i]) {
-          if (line != '') line += ','
-
-          line += array[i][index];
-        }
-
-        str += line + '\r\n';
-      }
-
-      var csv = array.map(function (row) {
-        return fields.map(function (fieldName) {
-          return JSON.stringify(row[fieldName])
-        }).join(',')
-      })
-      csv.unshift(fields.join(','))
-
-      return csv.join('\r\n');
-    },
-    download_csv: function () {
-      var raw = this.exportedData.rawData;
-      if (raw === 'false') {
-        var DATA = this.collectedData.refinedDF;
-      } else {
-        var DATA = this.collectedData.computedDF;
-      }
-      var csvContent = [];
-      csvContent = this.convertToCSV(DATA);
-
-      var hiddenElement = document.getElementById('Download');
-      hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvContent);
-      hiddenElement.target = '_blank';
-      hiddenElement.download = 'file.csv';
-      hiddenElement.click();
-    },
-    download_xml: function () {
-      alert("XML output not ready for the moment");
-      // c'est du node.js 'require'
-      // et ça marche pas :(
-      var convert = require('xml-js');
-      var raw = this.exportedData.rawData;
-      if (raw === 'false') {
-        var DATA = this.collectedData.refinedDF;
-      } else {
-        var DATA = this.collectedData.computedDF;
-      }
-      var xml_doc = convert.json2xml(DATA, options);
-
-      var hiddenElement = document.getElementById('Download');
-      hiddenElement.href = 'data:xml;charset=utf-8,' + encodeURI(xml_doc);
-      hiddenElement.target = '_blank';
-      hiddenElement.download = 'file.xml';
-      hiddenElement.click();
-    },
     tableSummarised: function () {
       var self = this;
       setTimeout(function () {
-        var raw = self.exportedData.rawData;
-        if (raw === "false") {
-          var df = self.collectedData.refinedDF;
-        } else {
-          var df = self.collectedData.computedDF
-        }
-
+        var df = self.collectedData.computedDF
         var colnames = Object.keys(df[0]);
         // create the JSON array for the columns required by DataTable
         var columns = [];
@@ -439,6 +185,7 @@ var App = new Vue({
       return (str);
     }
   },
+  /*  ou alors juste aller piquer du code depuis ce template */
   components: {
     'tab-compo': {
       inheritAttrs: true,
